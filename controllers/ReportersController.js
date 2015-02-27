7e5afe136d64be69f0e06ef57e470fdec6d2aff6
Matthew Terrill <meterrill@gmail.com>
2015-02-27 19:31:20 +0000

    add birthday and years of employment to reporters

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,7 +1,9 @@
 newspaperBeats.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId);
   $scope.addReporter = function() {
-    $scope.beat.reporters.push({name: $scope.reporterName});
+    $scope.beat.reporters.push({name: $scope.reporterName, birthday: $scope.birthday, yearsOfEmployment: $scope.yearsOfEmployment});
     $scope.reporterName = null;
+    $scope.birthday = null;
+    $scope.yearsOfEmployment = null;
   };
 });
